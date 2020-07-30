8ac843b4e5f96329a6ecaf25a116db2303a16d1d
s4vitar <s4vitar@localhost.localdomain>
2020-07-30 22:01:22 +0100

    Indentation WTF

--- downloadVPN.py
+++ downloadVPN.py
@@ -16,35 +16,34 @@ filename = sys.argv[1]
 
 if __name__ == '__main__':
 
-	try:
-		s = None
+    try:
+        s = None
+    	urllib3.disable_warnings()
+	s = requests.session()
+	s.verify = False
+	s.keep_alive = False
 
-		urllib3.disable_warnings()
-		s = requests.session()
-		s.verify = False
-		s.keep_alive = False
+	email = raw_input("Email: ")
+	password = getpass.getpass()
 
-		email = raw_input("Email: ")
-		password = getpass.getpass()
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
 
-		headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+	r = s.get(main_url, headers=headers)
+	token_value = re.findall(r'name="_token" value="(.*?)"', r.text)[0]
 
-		r = s.get(main_url, headers=headers)
-		token_value = re.findall(r'name="_token" value="(.*?)"', r.text)[0]
+        data_post = {
+           	'email': '%s' % (email),
+                'password': '%s' % (password),
+        	'_token': '%s' % (token_value)
+        }
 
-	        data_post = {
-	        	'email': '%s' % (email),
-	            'password': '%s' % (password),
-				'_token': '%s' % (token_value)
-	        }
+        r = s.post(main_url, data=data_post, headers=headers)
+        r = s.get(download_vpn_url, headers=headers)
 
-		r = s.post(main_url, data=data_post, headers=headers)
-		r = s.get(download_vpn_url, headers=headers)
+	f = open("%s" % filename, "w")
+    	f.write(r.text)
+	f.close()
 
-		f = open("%s" % filename, "w")
-		f.write(r.text)
-		f.close()
-
-	except:
-		sys.exit(1)
+    except:
+	sys.exit(1)
 
